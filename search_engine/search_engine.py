05b86193a64581199504776b546694d93bcf127c
developer <your.email@example.com>
2024-12-15 01:21:55 +0500

    add search function, add test

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -1,2 +1,28 @@
-def main(a, b):
-    return a + b
+def search(docs: list, target_string: str):
+    result = []
+    for doc in docs:
+        if target_string in doc['text'].split():
+            result.append(doc['id'])
+    return result
+
+
+# Текст документов
+doc1 = "I can't shoot straight unless I've had a pint!"
+doc2 = "Don't shoot shoot shoot that thing at me."
+doc3 = "I'm your shooter."
+
+# создание документа
+# документ имеет два атрибута "id" и "text"
+docs = [
+    {'id': 'doc1', 'text': doc1},
+    {'id': 'doc2', 'text': doc2},
+    {'id': 'doc3', 'text': doc3},
+]
+
+# поисковый движок проводит поиск
+result = search(docs, 'shoot')
+
+print(result)  # => ['doc1', 'doc2']
+
+# Документы пусты
+print(search([], 'shoot'))  # []
--- tests/test_search_engine.py
+++ tests/test_search_engine.py
@@ -1,5 +1,17 @@
 from search_engine import search_engine
 
+doc1 = "I can't shoot straight unless I've had a pint!"
+doc2 = "Don't shoot shoot shoot that thing at me."
+doc3 = "I'm your shooter."
 
-def test_main():
-    assert search_engine.main(2,2) == 4
+docs = [
+    {'id': 'doc1', 'text': doc1},
+    {'id': 'doc2', 'text': doc2},
+    {'id': 'doc3', 'text': doc3},
+]
+
+
+def test_search():
+    assert search_engine.search(docs, 'shoot') == ['doc1', 'doc2']
+    assert search_engine.search(docs, 'money') == []
+    assert search_engine.search([], 'shoot') == []
