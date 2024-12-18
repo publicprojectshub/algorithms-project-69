41d875a63f0dcbf4f454dbb0fa232792b0bef677
developer <your.email@example.com>
2024-12-19 02:56:52 +0500

    add input text processing

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -1,28 +1,16 @@
+import re
+
+
+def string_cleaner(input_string):
+    pattern = r'[!\"#$%&\'()*+,-./:;<=>?@[\\\]^_`{|}~]'
+    cleaned_string = re.sub(pattern, '', input_string, flags=re.IGNORECASE)
+    return cleaned_string
+
+
 def search(docs: list, target_string: str):
     result = []
     for doc in docs:
-        if target_string in doc['text'].split():
+        doc_cleaned_text = string_cleaner(doc['text']).lower().split()
+        if target_string.lower() in doc_cleaned_text:
             result.append(doc['id'])
     return result
-
-
-# Текст документов
-doc1 = "I can't shoot straight unless I've had a pint!"
-doc2 = "Don't shoot shoot shoot that thing at me."
-doc3 = "I'm your shooter."
-
-# создание документа
-# документ имеет два атрибута "id" и "text"
-docs = [
-    {'id': 'doc1', 'text': doc1},
-    {'id': 'doc2', 'text': doc2},
-    {'id': 'doc3', 'text': doc3},
-]
-
-# поисковый движок проводит поиск
-result = search(docs, 'shoot')
-
-print(result)  # => ['doc1', 'doc2']
-
-# Документы пусты
-print(search([], 'shoot'))  # []
--- tests/test_search_engine.py
+++ tests/test_search_engine.py
@@ -1,17 +1,27 @@
 from search_engine import search_engine
 
 doc1 = "I can't shoot straight unless I've had a pint!"
-doc2 = "Don't shoot shoot shoot that thing at me."
+doc2 = "Don't shoot_ shoot shoot that thing at me."
 doc3 = "I'm your shooter."
+doc4 = "You are _Shooter!."
 
 docs = [
     {'id': 'doc1', 'text': doc1},
     {'id': 'doc2', 'text': doc2},
     {'id': 'doc3', 'text': doc3},
+    {'id': 'doc4', 'text': doc4},
 ]
+
+input_str = "Hello, World! ;:>=-)_This is a test: @2023 #Python."
 
 
 def test_search():
     assert search_engine.search(docs, 'shoot') == ['doc1', 'doc2']
     assert search_engine.search(docs, 'money') == []
     assert search_engine.search([], 'shoot') == []
+    assert search_engine.search(docs, 'pint') == ['doc1']
+    assert search_engine.search(docs, 'shooter') == ['doc3', 'doc4']
+
+
+def test_string_cleaner():
+    assert search_engine.string_cleaner(input_str) == 'Hello World This is a test 2023 Python'
