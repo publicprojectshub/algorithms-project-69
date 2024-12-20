2b50f861fc74c818c5cb3b89683c25a543b35a54
developer <your.email@example.com>
2024-12-20 06:39:01 +0500

    add index_inverter function, add test

--- search_engine/search_engine.py
+++ search_engine/search_engine.py
@@ -1,35 +1,40 @@
 import re
-from collections import Counter
+from collections import Counter, defaultdict
 
 
 def tokenize(text):
-    # return list of split and lower text and digits
-    return re.findall(r'[a-zA-Z0-9]+', text.lower())
+    """return list of split and lower text and digits and apostrophes"""
+    return re.findall(r"[a-zA-Z0-9']+", text.lower())
 
 
 def calculate_relevance_score(doc_tokens, query_tokens):
     doc_counter = Counter(doc_tokens)
     query_counter = Counter(query_tokens)
-
     intersections = set(doc_counter) & set(query_counter)
     intersections_score = sum(doc_counter[word] * query_counter[word] for word in intersections)
-
     return intersections_score
 
 
+def index_inverter(docs_list: list):
+    """index_inverter return inverted words index like {'Hello':[doc2, doc3], 'world': [doc1, doc2]}"""
+    inverted_index = dict()
+    for doc in docs_list:
+        doc_id = doc["id"]
+        text_tokens = tokenize(doc["text"])
+        for token in text_tokens:
+            if token not in inverted_index:
+                inverted_index[token] = []
+            inverted_index[token].append(doc_id)
+    return inverted_index
+
+
 def search(docs: list, query: str):
-    query_tokens = tokenize(query)
-
-    # (doc id: score)
+    query_tokens = tokenize(query)  # (doc id: score)
     found_docs = []
-
     for doc in docs:
         doc_tokens = tokenize(doc['text'])
         score = calculate_relevance_score(doc_tokens, query_tokens)
-
         found_docs.append((doc['id'], score))
     found_docs.sort(key=lambda item: item[1], reverse=True)
     result = [doc_id for doc_id, score in found_docs if score > 0]
-
     return result
-
--- tests/test_search_engine.py
+++ tests/test_search_engine.py
@@ -6,10 +6,14 @@ doc3 = "I'm your shooter."
 doc4 = "You are _Shooter!."
 
 docs = [
-    {'id': 'doc1', 'text': doc1},
-    {'id': 'doc2', 'text': doc2},
-    {'id': 'doc3', 'text': doc3},
-    {'id': 'doc4', 'text': doc4},
+    {"id": "doc1", "text": doc1},
+    {"id": "doc2", "text": doc2},
+    {"id": "doc3", "text": doc3},
+    {"id": "doc4", "text": doc4},
+]
+
+docs2 = [
+    {"id": "doc1", "text": doc1}
 ]
 
 input_str = "Hello, World! ;:>=-)_This is a test: @2025 #Python."
@@ -32,3 +36,9 @@ def test_search_few_words():
 
 def test_tokenize():
     assert search_engine.tokenize(input_str) == ['hello', 'world', 'this', 'is', 'a', 'test', '2025', 'python']
+
+
+def test_index_inverter():
+    assert search_engine.index_inverter(docs2) == {'i': ['doc1'], "can't": ['doc1'], 'shoot': ['doc1'],
+                                                   'straight': ['doc1'], 'unless': ['doc1'], "i've": ['doc1'],
+                                                   'had': ['doc1'], 'a': ['doc1'], 'pint': ['doc1']}
